c4bb6d64feab3b8786f35241605252e3e0d0a6c6
Alvaro thinkpad p52 wsl fedora 42 <alvarojuanez.prgit01@gmail.com>
2025-09-19 23:16:07 -0400

    Programación c

---
+++ proyecto_de_grado/prog_C/Ejercicios/01_01_holaMundo/holaMundo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>          //Biblioteca estandar de entrada/salida
+
+int main(){                 //Función principal
+    printf("Hola Mundo\n");   //Imprime en pantalla
+    return 0;               //Termina el programa
+}
+
