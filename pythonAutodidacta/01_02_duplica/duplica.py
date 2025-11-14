fba47acdef63f75cde2a03e7edc11e101adf0936
thinkpad x230 fedora 42 <alvarojuanez.prgit01@gmail.com>
2025-11-14 00:26:08 -0400

    feat(math): Añadir función para duplicar un número
    refs: #Duplicador

---
+++ pythonAutodidacta/01_02_duplica/duplica.py
@@ -0,0 +1,14 @@
+import sys    #Biblioteca standar      
+
+def duplica():
+    
+    #Obten datos del usuario
+    numDuplicado: int = 0
+
+    numero = int(input("El doble de: "))    #Especificamos el tipo de dato
+    numDuplicado = numero * 2
+
+    #Imprime el resultado por pantalla
+    print("El numero duplicado es: ", numDuplicado)
+
+duplica()
