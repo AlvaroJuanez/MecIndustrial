b5762510d987fc9ad99e8570c6cf60e42b6cba9a
thinkpad x230 fedora 42 <alvarojuanez.prgit01@gmail.com>
2025-11-17 21:00:45 -0400

    feat(Programa-python): nombreApellido
    Con los datos adquiridos imprime por pantalla un mensaje
    
    Refs: #1

---
+++ pythonAutodidacta/01_03_nombre_apellido/nombreApellido.py
@@ -0,0 +1,14 @@
+import sys # Biblioteca standar
+
+def nombreApellido():
+
+    #Obten los datos del usuario
+    nombre = input("¿Cual es tu nombre?  \n")
+    apellido = input("¿y tu apellido?  \n")
+
+    #imprime por pantalla el nombre y el apellido
+    print("Hola " + nombre + " tengo un amigo que también es " + apellido)
+
+
+
+nombreApellido() #Llama a la funcion
